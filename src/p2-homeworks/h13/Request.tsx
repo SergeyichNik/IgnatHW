40c5dfea09983985f424f6c229bdb384b144dabf
SergeichNik <tyunev.nik89@gmail.com>
2022-05-03 21:43:41 +0300

    preloader add

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, useState} from 'react';
 import {requestApi} from "./RequestApi";
+import preloader from "../h10/Circles-menu-3.gif"
 
 const Request = () => {
 
@@ -12,17 +13,14 @@ const Request = () => {
     }
 
     const onClickHandler = () => {
-
         setIsFetching(true)
 
         requestApi.postCheckboxVal(isChecked)
-
             .then(res => {
                 setIsChecked(res.yourBody.success);
                 setMessage(res.errorText);
                 setIsFetching(false)
             })
-
             .catch(error => {
                 setMessage(error.response.data.errorText);
                 setIsFetching(false)
@@ -31,8 +29,11 @@ const Request = () => {
 
     return (
         <div>
-            <div>
-                {message}
+            <div style={{height: '50px', lineHeight: '50px'}}>
+                {isFetching
+                    ?<img style={{height: '50px'}} src={preloader} alt="preloader"/>
+                    :message
+                }
             </div>
             <input onChange={onChangeHandler} checked={isChecked} type="checkbox"/>
             <button disabled={isFetching} onClick={onClickHandler} >click</button>
